a1dbe298f4bd550c5c7adb2b54e318127be94af5
unknown <rabetohardy@gmail.com>
2022-06-01 11:54:50 +0100

    configuration de la page profil

--- Webapp/admin.py
+++ Webapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Utilisateur
+from .models import *
 
 # Register your models here.
 admin.site.register(Utilisateur)
+admin.site.register(News_Post)
+admin.site.register(Manage_Stress)
+admin.site.register(Ressources)
+admin.site.register(Commentaires)
