28ff17ba960040bc91ccf5d4186aca3e6b09b6e2
unknown <rabetohardy@gmail.com>
2022-08-17 19:44:07 +0300

    notification + nom dans django admin

--- Webapp/models.py
+++ Webapp/models.py
@@ -13,11 +13,17 @@ class Utilisateur(models.Model):
     Password = models.CharField(max_length=50)
     Status = models.CharField(max_length=20, choices=choix_status, default = 'Utilisateur Simple')
     
+    def __str__(self):
+        return self.Nom_Complet
+    
 class Commentaires(models.Model):
     Titre_pub = models.CharField(max_length=150)
     Auteur_Comment_pub = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
     Comment_pub = models.TextField(max_length=255)
     Date_Comment_pub = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.Titre_pub
     
     
 class Ressources(models.Model):
@@ -25,6 +31,9 @@ class Ressources(models.Model):
     Description_Ressource = models.TextField(max_length=255)
     Fichier_Ressource = models.FileField()
     Date_pub_Ressource = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.Titre_Ressource
 
 class Message(models.Model):
     User_send_msg = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
@@ -32,15 +41,24 @@ class Message(models.Model):
     Content_msg = models.TextField(max_length=255)
     Date_send_msg = models.DateTimeField(auto_now=True)
     Status_msg = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.Object_msg
 
 class Room(models.Model):
     Theme = models.CharField(max_length=150, blank=True)
+    
+    def __str__(self):
+        return self.Theme
     
 class Private_Message(models.Model):
     User = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
     Message = models.CharField(max_length=1000)
     Date = models.DateTimeField(auto_now=True)
     Room = models.CharField(max_length=150, blank=True)
+    
+    def __str__(self):
+        return self.Message
 
 
 class Manage_Stress(models.Model):
@@ -57,6 +75,9 @@ class News_Post(models.Model):
     Fichier = models.ImageField()
     Like = models.ManyToManyField(Utilisateur, default=None, blank=True, related_name='Reaction_Utilisateur')
     Auteur_pub = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.Titre
     
 class Assistance(models.Model):
     Cas = models.ForeignKey(Message, on_delete=models.CASCADE)
