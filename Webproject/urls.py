20c15cae08ed173ef79db1043ce892ca582a69dc
Rabeto Mac Manfred Hardy <rabetohardy@gmail.com>
2022-09-22 15:54:03 +0300

    resolution django admin appears

--- Webproject/urls.py
+++ Webproject/urls.py
@@ -20,6 +20,9 @@ from Webapp import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.autodiscover()
+admin.site.enable_nav_sidebar = False
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index_admin/', views.index_admin, name='index_admin'),
