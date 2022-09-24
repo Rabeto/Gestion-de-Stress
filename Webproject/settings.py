9f8debf580e7200e38776a5bac5e4d5dc602b3f3
Rabeto Mac Manfred Hardy <rabetohardy@gmail.com>
2022-09-24 19:25:04 +0300

    update settings and django.config

--- Webproject/settings.py
+++ Webproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8cl^zaqx5az98r08y(h8i!w@m8rw_ke^&&az-z1opem-waev8m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangoenv.eba-ruwykup3.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['djangoenv.eba-ruwykup3.us-west-2.elasticbeanstalk.com','127.0.0.1']
 
 
 # Application definition
@@ -118,9 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
