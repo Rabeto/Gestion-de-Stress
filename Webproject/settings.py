770fd4440f79ca797b85126fa9214d253403f998
Rabeto Mac Manfred Hardy <rabetohardy@gmail.com>
2022-09-24 14:25:08 +0300

    command for deploy prod

--- Webproject/settings.py
+++ Webproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Webapp',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,16 @@ TIME_ZONE = 'US/Pacific'
 USE_I18N = True
 
 USE_TZ = True
+
+if 'AWS_STORAGE_BUCKET_NAME' in os.environ:
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
+
+    AWS_S3_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_S3_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
 
 # Static files (CSS, JavaScript, Images)
